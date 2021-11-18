34c457d9b6500a60d4c0b5422b95faecc256dcfd
JuanMarulanda4 <79994699+Juan-789@users.noreply.github.com>
2021-11-18 18:36:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Write a program that asks a user for a three digit integer. Add the digits together and print the sum. 
+"""
+def sum():
+  nums = int(input("Give me a 3 digit number "))
+  one = nums % 10
+  deci = nums % 100 // 10
+  centi = (nums - deci) //100
+  print (one+deci+centi)
+sum()
